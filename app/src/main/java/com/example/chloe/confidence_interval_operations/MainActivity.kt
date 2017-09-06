f463ce0b5ca12ba8312093941c37ad3216e31c1d
Chloe <chloe@Chloes-MacBook-Pro.local>
2017-09-06 15:39:44 +0300

    added main layout to main activity with all data

---
+++ app/src/main/java/com/example/chloe/confidence_interval_operations/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.chloe.confidence_interval_operations
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/chloe/confidence_interval_operations/ToogleButtonGroupTableLayout.java
@@ -0,0 +1,73 @@
+package com.example.chloe.confidence_interval_operations;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class ToogleButtonGroupTableLayout extends TableLayout implements View.OnClickListener {
+
+    private static final String TAG = "ToggleButtonGroupTableLayout";
+    private RadioButton activeRadioButton;
+
+    /**
+     * @param context
+     */
+    public ToogleButtonGroupTableLayout(Context context) {
+        super(context);
+
+    }
+
+    /**
+     * @param context
+     * @param attrs
+     */
+    public ToogleButtonGroupTableLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    @Override public void onClick(View v) {
+        final RadioButton rb = (RadioButton) v;
+        if ( activeRadioButton != null ) {
+            activeRadioButton.setChecked(false);
+        }
+        rb.setChecked(true);
+        activeRadioButton = rb;
+    }
+
+
+    @Override public void addView(View child, int index,
+                        android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, index, params);
+        setChildrenOnClickListener((TableRow)child);
+    }
+
+
+
+    @Override public void addView(View child, android.view.ViewGroup.LayoutParams params) {
+        super.addView(child, params);
+        setChildrenOnClickListener((TableRow)child);
+    }
+
+
+    private void setChildrenOnClickListener(TableRow tr) {
+        final int c = tr.getChildCount();
+        for (int i=0; i < c; i++) {
+            final View v = tr.getChildAt(i);
+            if ( v instanceof RadioButton ) {
+                v.setOnClickListener(this);
+            }
+        }
+    }
+
+    public int getCheckedRadioButtonId() {
+        if ( activeRadioButton != null ) {
+            return activeRadioButton.getId();
+        }
+
+        return -1;
+    }
+}
